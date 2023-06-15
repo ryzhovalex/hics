facacdcb0ffd79abeb9fb111e653a8a5af96d9fa
ryzhovalex <thed4rkof@gmail.com>
2023-06-15 18:04:00 +0300

    feat: console magic

---
+++ Source/ConsoleMagic.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Slimebones.Hics {
+    /// <summary>
+    /// Various console research.
+    /// </summary>
+    class ConsoleMagic {
+        /// <summary>
+        /// Prints colors in the center of the console window.
+        /// </summary>
+        public static void PrintCenteredColors() {
+            ConsoleColor[] colors = {
+                ConsoleColor.Blue,
+                ConsoleColor.Red,
+                ConsoleColor.White,
+                ConsoleColor.Yellow
+            };
+
+            // clear to see first printed color correctly
+            Console.Clear();
+
+            foreach (ConsoleColor color in colors) {
+                Console.CursorLeft =
+                    (Console.BufferWidth - color.ToString().Length) / 2;
+                Console.CursorTop =
+                    (Console.BufferHeight / 2);
+
+                Console.ForegroundColor = color;
+                Console.WriteLine(color);
+
+                Thread.Sleep(1000);
+
+                Console.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Echoes all input back to console.
+        /// </summary>
+        public static void Parrot() {
+            string? message = "";
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Talk to Parrot! To quit type \"q\".");
+            Console.WriteLine();
+
+            while (message != "q") {
+                Console.ForegroundColor = ConsoleColor.White;
+                message = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(message);
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -3,6 +3,15 @@
 namespace Slimebones.Hics {
     class Program {
         static void Main(string[] args) {
+            // CallQuickSort();
+            CallConsoleMagic();
+        }
+
+        static void CallConsoleMagic() {
+            ConsoleMagic.Parrot();
+        }
+
+        static void CallQuickSort() {
             int[] numbers = {10, 98, 78, 4, 54, 25, 41, 30, 87, 60, 84, 6, 12};
 
             Utils.Printer.PrintArray(numbers);
